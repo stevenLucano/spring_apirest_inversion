3dba96293cf61ee635d12a8b1288506819e86058
stLucano <st.lucano1205@gmail.com>
2023-12-08 20:42:57 -0500

    Proyecto inicializado

---
+++ src/main/java/co/com/sl/inversiones/Reto2Application.java
@@ -0,0 +1,13 @@
+package co.com.sl.inversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sl/inversiones/Reto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sl.inversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
